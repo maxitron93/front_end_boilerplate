b25908fa3066be3b9d5a2ba0acf54f957358733a
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-08-17 11:59:27 +1000

    Add clarification on how to add images

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import 'normalize.css/normalize.css';
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import '../scss/styles.scss';
-import KittenImage from '../img/kitten.jpg'; // This is how to add images
+import KittenImage from '../img/kitten.jpg'; // Images need to be imported through js so webpack will take them and put them into /dist/img. It's important that the path from index.html to images in /src is the same as the path from index.html to images in /dist
 import { testImportFunc } from './components/test' // This is how to import from other modules
 
 console.log('test')
